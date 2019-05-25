fbd2574406ebd69c7d9c0eb27c148f26fb65fe99
53 <gt_vengeance@yahoo.co.jp>
2019-05-25 12:58:35 +0900

    Initial Project Commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import Todo from "./todo";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  modules: {
+    Todo
+  }
 });
